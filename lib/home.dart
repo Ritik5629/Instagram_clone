525442f7403f0004f3907f2dbe9710fd70725920
Ritik5629 <ritikrana2804@gmail.com>
2025-12-18 13:52:30 +0530

    Bottom Bar Added in This GUI

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  final List<Widget> screens = [
+      HomePage(),
+      Center(child: Text("Page is Coming soon",style: TextStyle(color: Colors.green),),),
+      Center(child: Text("Page is Coming soon",style: TextStyle(color: Colors.green),),),
+      Center(child: Text("Page is Coming soon",style: TextStyle(color: Colors.green),),),
+      Center(child: Text("Page is Coming soon",style: TextStyle(color: Colors.green),),),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,252 +35,258 @@ class HomePage extends StatelessWidget {
           actions: [
             IconButton(
               onPressed: () {},
-              icon: const Icon(
-                Icons.add,
-                color: Colors.black,
-              ),
+              icon: const Icon(Icons.add, color: Colors.black),
             ),
             SizedBox(width: 10),
             IconButton(
               onPressed: () {},
-              icon: const Icon(
-                Icons.favorite_border,
-                color: Colors.black,
-              ),
+              icon: const Icon(Icons.favorite_border, color: Colors.black),
             ),
-
             SizedBox(width: 10),
             IconButton(
               onPressed: () {},
-              icon: const Icon(
-                Icons.chat_bubble_outline,
-                color: Colors.black,
-              ),
+              icon: const Icon(Icons.chat_bubble_outline, color: Colors.black),
             ),
             SizedBox(width: 10),
           ],
         ),
 
-
-        
-        body: SizedBox(
-          height: 100,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              Row(
+        body: ListView(
+          children: [
+            // STORIES SECTION
+            SizedBox(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
                 children: [
-              
-                   SizedBox(width:5),
-                  Column(
+                  Row(
                     children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
+                      SizedBox(width: 5),
+                      Column(
+                        children: [
+                          CircleAvatar(radius: 35),
+                          SizedBox(height: 9),
+                          Text("Your Story"),
+                        ],
                       ),
-                    SizedBox(height: 9),
-                    Text("Your Story"),
-                    ],
-                    
-                    
-                  ),
-              
-                   SizedBox(width:6),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
-                    ],
-                    
-                    
-                  ),
-                  SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
-                    ],
-                    
-                    
-                  ),
-                  SizedBox(width:6),
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
-                    ],
-                    
-                    
-                  ),
-                   SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                   Text("ABYZ"),
-                    ],
-                    
-                    
-                  ), 
-              
-                  SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
-                    ],
-                    
-                    
-                  ),
-                  SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
-                    ],
-                    
-                    
-                  ),    
 
-                  SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
-                    ],
-                    
-                    
-                  ), 
-                  SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
-                    ],
-                    
-                    
-                  ), 
-                  SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
-                      ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
-                    ],
-                    
-                    
-                  ), 
+                      SizedBox(width: 6),
 
-                   SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
+                      Column(
+                        children: [
+                          CircleAvatar(radius: 35),
+                          SizedBox(height: 9),
+                          Text("ABYZ"),
+                        ],
                       ),
-                    SizedBox(height: 9),
-                   Text("ABYZ"),
-                    ],
-                    
-                    
-                  ), 
-                   SizedBox(width:5),
-              
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        
-                        radius: 35,
-                      
-                                    
-                                    
+
+                      SizedBox(width: 5),
+
+                      Column(
+                        children: [
+                          CircleAvatar(radius: 35),
+                          SizedBox(height: 9),
+                          Text("ABYZ"),
+                        ],
                       ),
-                    SizedBox(height: 9),
-                    Text("ABYZ"),
                     ],
-                    
-                    
-                  ),                                                                                                                 
+                  ),
                 ],
               ),
-            ],
-          ),
+            ),
+
+            SizedBox(height: 13),
+
+            // POST = > 1
+            Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("ABCVR"),
+                    trailing: Icon(Icons.more_horiz),
+                  ),
+
+                  Image.asset("assets/images/ice_cream_post.jfif"),
+
+                  // Row(
+                  //   children: [
+                  //     ListTile(
+                  //       leading: Icon(
+                  //         Icons.favorite_border,
+                  //       ),
+                  //     )
+                  //   ],
+                  // )
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite_border),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.comment_outlined),
+                      ),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+
+                      Spacer(), // pushes save icon to the right
+
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.bookmark_border),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            // => POST 2
+            Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("ABCVR"),
+                    trailing: Icon(Icons.more_horiz),
+                  ),
+
+                  Image.asset("assets/images/ice_cream_post.jfif"),
+
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite_border),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.comment_outlined),
+                      ),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+
+                      Spacer(),
+
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.bookmark_border),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("ABCVR"),
+                    trailing: Icon(Icons.more_horiz),
+                  ),
+
+                  Image.asset("assets/images/ice_cream_post.jfif"),
+
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite_border),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.comment_outlined),
+                      ),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+
+                      Spacer(),
+
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.bookmark_border),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("ABCVR"),
+                    trailing: Icon(Icons.more_horiz),
+                  ),
+
+                  Image.asset("assets/images/ice_cream_post.jfif"),
+
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite_border),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.comment_outlined),
+                      ),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.send)),
+
+                      Spacer(),
+
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.bookmark_border),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+            icon: Icon(Icons.home,color: Colors.green,),
+            label: "Home",
+            
+            ),
+            BottomNavigationBarItem(
+            icon: Icon(Icons.search,color: Colors.green),
+            label: "Search",
+            
+            ),
+             BottomNavigationBarItem(
+            icon: Icon(Icons.video_library,color: Colors.green),
+            label: "Reels",
+            
+            ),
+             BottomNavigationBarItem(
+            icon: Icon(Icons.add,color: Colors.green),
+            label: "Post",
+            ),       
+            BottomNavigationBarItem(
+            icon: Icon(Icons.person,color: Colors.green),
+            label: "Post",
+            ),             
+
+          ],
         ),
       ),
     );
